17f8cdc20b99f69a6697a6342ab5dca15d23f938
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-10-29 11:30:52 -0300

    feat:🎸 home content

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,21 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels'
+import { CodeBlock } from '../components/ui/code-block'
+import { SectionDataTable } from '../components/ui/section-data-table'
+import { SectionTitle } from '../components/ui/section-title'
 import { Sidebar } from '../components/ui/sidebar'
+import { WebhooksDetailHeader } from '../components/webhooks-detail-header'
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
+
+const overviewData = [
+  { key: 'Method', value: 'POST' },
+  { key: 'Status Code', value: '200' },
+  { key: 'Content-type', value: 'application/json' },
+  { key: 'Content-length', value: '23482343 bytes' },
+]
 
 function Index() {
   return (
@@ -17,7 +28,35 @@ function Index() {
         <PanelResizeHandle className="w-px bg-zinc-700 hover:bg-zinc-600 transition-colors duration-150" />
 
         <Panel defaultSize={80} minSize={60}>
-          main content
+          <div className="flex h-full flex-col">
+            <WebhooksDetailHeader
+              ip="192.168.1.1"
+              timestamp="April 18th 14pm"
+            />
+
+            {/* Main content */}
+            <div className="flex-1 overflow-y-auto p-6 space-y-6">
+              <div className="space-y-4">
+                <SectionTitle>Request Overview</SectionTitle>
+                <SectionDataTable data={overviewData} />
+              </div>
+
+              <div className="space-y-4">
+                <SectionTitle>Query Parameters</SectionTitle>
+                <SectionDataTable data={overviewData} />
+              </div>
+
+              <div className="space-y-4">
+                <SectionTitle>Headers</SectionTitle>
+                <SectionDataTable data={overviewData} />
+              </div>
+
+              <div className="space-y-4">
+                <SectionTitle>Request Body</SectionTitle>
+                <CodeBlock code={JSON.stringify(overviewData, null, 2)} />
+              </div>
+            </div>
+          </div>
         </Panel>
       </PanelGroup>
     </div>
