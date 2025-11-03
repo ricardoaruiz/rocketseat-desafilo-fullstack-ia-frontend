469292233fc31c612ba9552236a19fdaa0f6ca86
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-11-03 08:45:12 -0300

    feat: 🎸 add infinite scroll on webhook list using InfiniteQuery

--- src/components/webhooks-list.tsx
+++ src/components/webhooks-list.tsx
@@ -1,17 +1,61 @@
-import { useSuspenseQuery } from '@tanstack/react-query'
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query'
+import { Loader2 } from 'lucide-react'
+import { useEffect, useRef } from 'react'
 import { WEBHOOK_LIST_SCHEMA } from '../http/schemas/webhooks'
 import { WebhooksListItem } from './webhooks-list-item'
 
 export function WebhooksList() {
-  const { data } = useSuspenseQuery({
-    queryKey: ['webhooks'],
-    queryFn: async () => {
-      const response = await fetch('http://localhost:3334/api/webhooks')
-      const data = await response.json()
+  const loadMoreRef = useRef<HTMLDivElement | null>(null)
+  const observerRef = useRef<IntersectionObserver | null>(null)
 
-      return WEBHOOK_LIST_SCHEMA.parse(data)
-    },
-  })
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useSuspenseInfiniteQuery({
+      queryKey: ['webhooks'],
+      queryFn: async ({ pageParam }) => {
+        const url = new URL('http://localhost:3334/api/webhooks')
+        if (pageParam) {
+          url.searchParams.set('cursor', pageParam)
+        }
+        const response = await fetch(url)
+        const data = await response.json()
+
+        return WEBHOOK_LIST_SCHEMA.parse(data)
+      },
+      initialPageParam: undefined as string | undefined,
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+      select: (data) => ({
+        webhooks: data.pages.flatMap((page) => page.webhooks),
+        nextCursor: data.pages[data.pages.length - 1].nextCursor,
+      }),
+    })
+
+  useEffect(() => {
+    if (loadMoreRef.current) {
+      observerRef.current?.disconnect()
+    }
+
+    observerRef.current = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0]
+        if (entry.isIntersecting && hasNextPage && !isFetchingNextPage) {
+          fetchNextPage()
+        }
+      },
+      {
+        threshold: 0.1,
+      },
+    )
+
+    if (loadMoreRef.current) {
+      observerRef.current.observe(loadMoreRef.current)
+    }
+
+    return () => {
+      if (loadMoreRef.current) {
+        observerRef.current?.disconnect()
+      }
+    }
+  }, [hasNextPage, isFetchingNextPage, fetchNextPage])
 
   return (
     <div className="flex-1 overflow-y-auto">
@@ -20,6 +64,16 @@ export function WebhooksList() {
           <WebhooksListItem key={webhook.id} data={webhook} />
         ))}
       </div>
+
+      {hasNextPage && (
+        <div className="p-2" ref={loadMoreRef}>
+          {isFetchingNextPage && (
+            <div className="flex items-center justify-center">
+              <Loader2 className="size-5 animate-spin" />
+            </div>
+          )}
+        </div>
+      )}
     </div>
   )
 }
